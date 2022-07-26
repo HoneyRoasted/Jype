333f1eafb541a6b193e19df330f0907ba14c66a2
John-Ryan Dunn <socraticphoenix@gmail.com>
2022-07-26 13:35:17 -0500

    bump

--- src/main/java/honeyroasted/jype/Namespace.java
+++ src/main/java/honeyroasted/jype/Namespace.java
@@ -1,4 +1,19 @@
 package honeyroasted.jype;
 
+import java.util.Arrays;
+
 public class Namespace {
+    private String[] path;
+    private String name;
+
+    public Namespace(String[] path, String name) {
+        this.path = path;
+        this.name = name;
+    }
+
+    public Namespace(String... elements) {
+        this.path = Arrays.copyOfRange(elements, 0, elements.length - 1);
+        this.name = elements[elements.length - 1];
+    }
+
 }
