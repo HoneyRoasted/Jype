216447b15f5b0f34ddc1362767988c03b1436402
John-Ryan Dunn <socraticphoenix@gmail.com>
2024-07-10 15:38:35 -0400

    Add license

--- src/main/java/honeyroasted/jype/system/solver/_old/solvers/inference/helper/AbstractInferenceHelper.java
+++ src/main/java/honeyroasted/jype/system/solver/_old/solvers/inference/helper/AbstractInferenceHelper.java
@@ -1,6 +1,5 @@
 package honeyroasted.jype.system.solver._old.solvers.inference.helper;
 
-import honeyroasted.jype.system.TypeOperations;
 import honeyroasted.jype.system.solver.TypeSolver;
 import honeyroasted.jype.system.solver.solvers.NoOpTypeSolver;
 
